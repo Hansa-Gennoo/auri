722c1c6f8b98b09335a570fa3db6b7718414f14e
Hansa Gennoo <hansagennoo10@gmail.com>
2025-11-04 16:14:55 +0400

    added username to user and updated pageges controller to manage user sessions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,20 @@
-class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
 
-  def home
+  protected
+
+  def after_sign_in_path_for(resource)
+    landing_page = resource.landing_page || resource.create_landing_page!(title: "My Landing Page")
+    dashboard_landing_page_path(landing_page)
+  end
+
+
+ def after_sign_up_path_for(resource)
+    after_sign_in_path_for(resource)
+  end
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :display_name])
   end
 end
