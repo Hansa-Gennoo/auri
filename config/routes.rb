50cfab78d0bf589c94f27b51e2b90bdfa5dd82e7
Hansa Gennoo <hansagennoo10@gmail.com>
2025-11-04 15:51:05 +0400

    created routes for all models

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,22 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :dashboard do
+    resources :landing_pages, only: [:show, :edit, :update] do
+      resources :links, only: [:index, :new, :create, :edit, :update, :destroy]
+    end
+
+  end
+  resources :themes
+  devise_for :users
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # Public landing pages (username-based)
+  #get '/:username', to: 'landing_pages#show', as: :public_landing_page
 end
