9d060f31a9b6ccd8a164a71b8b0b680bb7dc7ead
Hansa Gennoo <hansagennoo10@gmail.com>
2025-11-04 15:47:25 +0400

    added associations and validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
   belongs_to :landing_page
+
+  validates :title, :url, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,27 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+   has_one :landing_page, dependent: :destroy
+
+  after_create :create_default_landing_page
+
+  def create_default_landing_page
+    default_theme = Theme.first
+
+    create_landing_page(title: "My First Landing Page", theme: Theme.first)
+  end
+
+ # extend FriendlyId
+  #friendly_id :username, use: :slugged
+
+
+
+  #def should_generate_new_friendly_id?
+    #slug.blank? || will_save_change_to_username?
+  #end
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
